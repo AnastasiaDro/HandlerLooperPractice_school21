b67141d4fe975c37c83586acd8fe7a58d9293a55
Anastasia Drogunova <a.drogunova@vk.team>
2024-05-27 00:59:39 +0300

    workpiece

---
+++ app/src/main/java/com/school21/handlerlooper/HandlerLooperStuff.kt
@@ -0,0 +1,7 @@
+package com.school21.handlerlooper
+
+import android.util.Log
+
+val TAG = "SCHOOL_21: "
+
+//Каждое сообщение закреплено за хендлером
--- app/src/main/java/com/school21/handlerlooper/MainActivity.kt
+++ app/src/main/java/com/school21/handlerlooper/MainActivity.kt
@@ -2,10 +2,30 @@ package com.school21.handlerlooper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var startButton: Button
+    lateinit var stopButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startButton = findViewById(R.id.button_start)
+        stopButton = findViewById(R.id.button_stop)
+
+        //запустим поток
+        startButton.setOnClickListener {
+            Log.d(TAG, "LooperThread started")
+        }
+
+        //остановим поток
+        stopButton.setOnClickListener {
+            Log.d(TAG, "LooperThread stopped")
+        }
     }
+
 }
