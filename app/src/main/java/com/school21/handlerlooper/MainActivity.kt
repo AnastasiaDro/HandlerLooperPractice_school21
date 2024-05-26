7f91b143614ecae5f5088569e5510b856f66cb74
Anastasia Drogunova <a.drogunova@vk.team>
2024-05-27 00:22:02 +0300

    Initial commit

---
+++ app/src/main/java/com/school21/handlerlooper/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.school21.handlerlooper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
