7a4bb662338622b1674104e2132e85fe1beb1b3f
Anastasia Drogunova <a.drogunova@vk.team>
2024-06-02 22:32:49 +0300

    created test project

--- app/src/main/java/com/school21/handlerlooper/HandlerLooperStuff.kt
+++ app/src/main/java/com/school21/handlerlooper/HandlerLooperStuff.kt
@@ -1,7 +1,63 @@
 package com.school21.handlerlooper
 
+import android.os.Looper
 import android.util.Log
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.LinkedBlockingQueue
 
 val TAG = "SCHOOL_21: "
 
 //Каждое сообщение закреплено за хендлером
+class CustomLooper(val thread: LooperThread) {
+
+    private val messageQueue: BlockingQueue<Message> = LinkedBlockingQueue()
+
+    fun loop() {
+        while (true) {
+            try {
+                if (!thread.isInterrupted) {
+                    synchronized(lock = this) {
+                        Log.d(TAG, "Запустили поток")
+                        (this as? Object)?.wait(1000L)
+                        val msg = messageQueue.take()
+                        msg.runnable.run()
+                        msg.handler.handleMessage(msg)
+                    }
+                }
+            } catch (e: InterruptedException) {
+                messageQueue.clear()
+                thread.interrupt()
+            }
+        }
+    }
+
+    fun addMessage(message: Message) {
+        messageQueue.put(message)
+    }
+
+    fun quit() {
+        thread.interrupt()
+    }
+}
+
+class Message(val handler: CustomHandler, val description: String, val runnable: Runnable)
+
+class CustomHandler(val looper: CustomLooper) {
+    //Вызывается, когда поток обработал наше сообщение
+    fun handleMessage(msg: Message) {
+        Log.d(TAG, "Обработали сообщение ${msg.description}")
+    }
+
+    fun post(message: Message) {
+        looper.addMessage(message)
+    }
+}
+
+class LooperThread : Thread("LooperThread") {
+
+    val looper = CustomLooper(this)
+
+    override fun run() {
+        looper.loop()
+    }
+}
--- app/src/main/java/com/school21/handlerlooper/MainActivity.kt
+++ app/src/main/java/com/school21/handlerlooper/MainActivity.kt
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var startButton: Button
     lateinit var stopButton: Button
+    lateinit var text: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,15 +18,35 @@ class MainActivity : AppCompatActivity() {
 
         startButton = findViewById(R.id.button_start)
         stopButton = findViewById(R.id.button_stop)
+        text = findViewById(R.id.textView)
 
         //запустим поток
+        val thread = LooperThread()
+        val handler = CustomHandler(thread.looper)
+        thread.start()
+
+        var c = 0
         startButton.setOnClickListener {
+
             Log.d(TAG, "LooperThread started")
+            var a =c + 10
+            while (c < a) {
+                handler.post(Message(handler, "I am desription $c") {
+                    //handleMessage
+                    println("${Thread.currentThread().name} maked runnable number $c")
+                })
+                c++
+            }
         }
 
         //остановим поток
         stopButton.setOnClickListener {
             Log.d(TAG, "LooperThread stopped")
+            thread.looper.quit()
+        }
+
+        text.setOnClickListener {
+            text.postDelayed({ println("Hello world was pressed!!") }, 5000L)
         }
     }
 
